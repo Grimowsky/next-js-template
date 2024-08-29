4f20bd594eb230e1af3fa94d6540ede6b77478e4
Grimm <rafalmirkowski@gmail.com>
2024-08-29 14:24:36 +0200

    add shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-  return <> hello </>;
+  return (
+    <>
+      <div className="p-10">
+        <Button> hello there x2 </Button>
+      </div>
+    </>
+  );
 }
