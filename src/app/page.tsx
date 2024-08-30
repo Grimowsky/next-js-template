ec2e25b1373996b9543f63ab94c1b936c6c702e2
Grimm <rafalmirkowski@gmail.com>
2024-08-30 11:11:28 +0200

    finish template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-import { Button } from '@/components/ui/button';
-
 export default function Home() {
-  return (
-    <>
-      <div className="p-10">
-        <Button> hello there x2 </Button>
-      </div>
-    </>
-  );
+  return <>hello world</>;
 }
